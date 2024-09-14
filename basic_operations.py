1e7a3bb370cd4ebf7e8e142580b525e873f2322e
Dagitt55 <50029583+Dagitt55@users.noreply.github.com>
2024-09-14 23:42:34 +0300

    Add files via upload

---
+++ basic_operations.py
@@ -0,0 +1,13 @@
+# Assign values to the variables
+number1 = int(input("number1 "))
+number2 = int(input("number2 "))
+
+# Perform the operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+# Print the results
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
