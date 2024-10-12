583da207482a7eaac88091f2fcb35210e3aa4c46
Dagitt55 <50029583+Dagitt55@users.noreply.github.com>
2024-10-12 23:36:34 +0300

    Add files via upload

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,15 @@
+import math
+class Shape:
+    def area(self):
+        pass
+class Rectangle(Shape):
+    def __init__(self, length: float, width: float):
+        self.length = length
+        self.width = width
+    def area(self):
+        return self.length * self.width
+class Circle(Shape):
+    def __init__(self, radius: float):
+        self.radius = radius
+    def area(self):
+        return math.pi * self.radius * self. radius
